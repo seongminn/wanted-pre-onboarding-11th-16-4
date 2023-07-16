ea45fedb666a16c60bc2231c4ab20a3ba3d260d2
seongminn <berry9901@gmail.com>
2023-07-17 01:14:37 +0900

    feat: Search Page 구현
    feat: Layout 컴포넌트 생성
    feat: 검색어 api 호출 기능 구현
    style: search page 스타일 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,47 @@
+import { ChangeEvent, useEffect, useState } from 'react';
+
+import { getSick } from '@/apis/sick';
+import Layout from '@/components/common/Layout';
+import { Sick } from '@/types/sick';
+
 function App() {
-  return <div>메인입니다.</div>;
+  const [sicks, setSicks] = useState<Sick[] | null>(null);
+  const [input, setInput] = useState<string>('');
+
+  const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
+  useEffect(() => {
+    getSick({ q: input }).then((res) => setSicks(res));
+  }, [input]);
+
+  return (
+    <Layout>
+      <form className="relative h-auto max-w-[800px] w-full">
+        <input
+          value={input}
+          onChange={onChangeInput}
+          className="w-full text-xl border-2 rounded-full p-7"
+          placeholder="질환명을 입력해주세요."
+        />
+        <div className="absolute w-full pt-5 mt-5 overflow-auto bg-white border-2 rounded-2xl min-h-[400px] max-h-96 top-full">
+          <span className="text-sm text-gray-400 px-7">추천 검색어</span>
+          {sicks?.length ? (
+            <ul className="pt-3">
+              {sicks?.map((sick) => (
+                <li key={sick.sickCd} className="py-2 px-7 hover:bg-slate-50">
+                  {sick.sickNm}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <div className="py-2 text-gray-400 px-7">검색어 없음</div>
+          )}
+        </div>
+      </form>
+    </Layout>
+  );
 }
 
 export default App;
