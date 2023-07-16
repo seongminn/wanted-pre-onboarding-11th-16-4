c443658fbaadda2e6abe759f333801fa99de4c1b
seongminn <berry9901@gmail.com>
2023-07-16 17:13:55 +0900

    :tada: feat: 초기 세팅

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>메인입니다.</div>;
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import './index.css';
+
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App.tsx';
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
