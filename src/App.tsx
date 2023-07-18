1f6e154879c40089ddde55433de7ffe8e6d4b42f
seongminn <berry9901@gmail.com>
2023-07-18 23:12:09 +0900

    refactor: 컴포넌트 분할
    feat: SearchInput, SearchList, SearchItem 컴포넌트 생성
    feat: SearchIcon 생성
    style: 검생창 스타일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import { getSick } from '@/apis/sick';
 import Layout from '@/components/common/Layout';
 import { Sick } from '@/types/sick';
 
+import SearchInput from './components/common/search/SearchInput';
+import SearchList from './components/common/search/SearchList';
 import useDebouncedValue from './hooks/useDebounce';
 
 function App() {
-  const [sicks, setSicks] = useState<Sick[] | null>(null);
+  const [sicks, setSicks] = useState<Sick[]>([]);
   const [input, setInput] = useState<string>('');
 
-  const debounceOnChangeInput = useDebouncedValue<string>(input, 200);
+  const debounceOnChangeInput = useDebouncedValue<string>(input, 350);
 
   const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
@@ -26,27 +28,9 @@ function App() {
 
   return (
     <Layout>
-      <form className="relative h-auto max-w-[800px] w-full">
-        <input
-          value={input}
-          onChange={onChangeInput}
-          className="w-full text-xl border-2 rounded-full p-7"
-          placeholder="질환명을 입력해주세요."
-        />
-        <div className="absolute w-full pt-5 mt-5 overflow-auto bg-white border-2 rounded-2xl min-h-[400px] max-h-96 top-full">
-          <span className="text-sm text-gray-400 px-7">추천 검색어</span>
-          {sicks?.length ? (
-            <ul className="pt-3">
-              {sicks?.map((sick) => (
-                <li key={sick.sickCd} className="py-2 px-7 hover:bg-slate-50">
-                  {sick.sickNm}
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <div className="py-2 text-gray-400 px-7">검색어 없음</div>
-          )}
-        </div>
+      <form className="relative h-auto max-w-[700px] w-full">
+        <SearchInput input={input} onChangeInput={onChangeInput} />
+        {input && <SearchList sicks={sicks} />}
       </form>
     </Layout>
   );
