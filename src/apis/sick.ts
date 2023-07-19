5b978d740b61376b70b00be75e1b8414cd544fea
seongminn <berry9901@gmail.com>
2023-07-19 14:47:20 +0900

    :recycle: refactor: debounce를 적용하여 api 호출 최적화
    feat: useDebounce 훅 함수 생성
    fix: input이 빈 문자열일 때 모든 data를 반환하는 버그 수정
    chore: api 호출 시 콘솔창에 "calling api"를 출력

--- src/apis/sick.ts
+++ src/apis/sick.ts
@@ -3,7 +3,11 @@ import { Sick } from '@/types/sick';
 import { client } from '.';
 
 export const getSick = async (params: { q: string }) => {
+  if (!params.q) return [];
+
   const { data } = await client.get<Sick[]>('sick', { params });
+
+  console.info('calling api');
 
   return data;
 };
